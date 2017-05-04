f86ef360790eb3374eaf6aa60c2c1b7b369a12b6
Larissa Gaulia <larissagaulia@gmail.com>
2017-05-04 17:09:26 -0300

    events attached as properties of components trigger methods

--- src/index.js
+++ src/index.js
@@ -9,6 +9,32 @@ ReactDOM.render(
 );
 
 
+
+var Counter = React.createClass({
+  incrementCount: function(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+  getInitialState: function(){
+     return {
+       count: 0
+     }
+  },
+  render: function(){
+    return (
+      <div class="my-component">
+        <h1>Count: {this.state.count}</h1>
+        <button type="button" onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Counter/>, document.getElementById('mount-point'));
+
+
+
 var MyComponent = React.createClass({
     getInitialState: function(){
         return {
