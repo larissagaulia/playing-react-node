9aeb7e030802d61b9bedba6b3e2797ffb4d03e38
Larissa Gaulia <larissagaulia@gmail.com>
2017-05-04 18:28:07 -0300

    unidirectional flow

--- src/index.js
+++ src/index.js
@@ -7,8 +7,75 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+/**************************************************************/
+/*
+In React, application data flows unidirectionally via the state and props objects, 
+as opposed to the two-way binding of libraries like Angular. 
+This means that, in a multi component heirachy, a common parent component should manage the state 
+and pass it down the chain via props.
 
+state should be updated using setState method to ensure  UI refresh. 
+The resulting values should be passed down to child components using attributes that are accessible 
+in said children via this.props.
 
+*/
+var FilteredList = React.createClass({
+  filterList: function(event){
+    
+    var updatedList = this.state.initialItems;
+
+    updatedList = updatedList.filter(function(item){
+      return item.toLowerCase().search(
+        event.target.value.toLowerCase()) !== -1;
+    });
+
+    this.setState({items: updatedList});
+  },
+  getInitialState: function(){
+     return {
+       initialItems: [
+         "Apples",
+         "Broccoli",
+         "Chicken",
+         "Duck",
+         "Eggs",
+         "Fish",
+         "Granola",
+         "Hash Browns"
+       ],
+       items: []
+     }
+  },
+  componentWillMount: function(){
+    this.setState({items: this.state.initialItems})
+  },
+  render: function(){
+    return (
+      <div className="filter-list">
+        <input type="text" placeholder="Search" onChange={this.filterList}/>
+      <List items={this.state.items}/>
+      </div>
+    );
+  }
+});
+
+var List = React.createClass({
+  render: function(){
+    return (
+      <ul>
+      {
+        this.props.items.map(function(item) {
+          return <li key={item}>{item}</li>
+        })
+       }
+      </ul>
+    )  
+  }
+});
+
+ReactDOM.render(<FilteredList/>, document.getElementById('mount-point'));
+
+/**************************************************************
 
 var Counter = React.createClass({
   incrementCount: function(){
@@ -33,7 +100,7 @@ var Counter = React.createClass({
 
 ReactDOM.render(<Counter/>, document.getElementById('mount-point'));
 
-
+*************************************************************/
 
 var MyComponent = React.createClass({
     getInitialState: function(){
